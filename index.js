b16918dca4535c12476cfae5e03b5f2986dd327d
Breno Henrique <brenosc2@hotmail.com>
2018-09-25 11:18:21 -0300

    adding comments

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import styled from "styled-components";
 
+// Criando uma tag <h1> estilizada
 const Greeting = styled.h1`
   text-align: center;
   color: blue;
 `;
 
+// Criando um wrapper que exibe o nome passado para o Greeting criado acima
 const Hello = ({ name }) => <Greeting>{name}</Greeting>;
 
+// Vinculando o componente Hello com a o nó de id = root (olha lá no index.hml)
 render(
   <Hello name="Meu primeiro componente!" />,
   document.getElementById("root")
