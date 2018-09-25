30178a234f306705a93ecb6476dfb1fd3beac5e1
Breno Henrique <brenosc2@hotmail.com>
2018-09-25 11:21:23 -0300

    adding comments

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+// Essa é a notação de classes usada para criar componentes
 class HomeScreen extends React.Component {
 
+  // Primeiro hook executado pelo React
   constructor(props) {
     super(props)
+
+    // Definição do estado local inicial do componente
     this.state = { message: '' }
   }
 
+  // Hook executado após o constructor
   componentDidMount () {
     this.setState({ message: this.props.message })
     setTimeout(() => this.setState({ message: '@@@ Meu segundo componente! @@@ ' }), 3000);
